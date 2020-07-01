3e7d8c20075d9e9eec0e67c00bad38cff350390e
alexey@tsenekov.ru <tsenekov>
2020-07-01 03:55:03 +0300

    Update

---
+++ Main.dfm
@@ -0,0 +1,43 @@
+object Form4: TForm4
+  Left = 0
+  Top = 0
+  BorderIcons = [biSystemMenu, biMinimize]
+  BorderStyle = bsSingle
+  Caption = 'Demo'
+  ClientHeight = 123
+  ClientWidth = 589
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poScreenCenter
+  OnCloseQuery = FormCloseQuery
+  PixelsPerInch = 96
+  TextHeight = 13
+  object GaugeAni: TGauge
+    Left = 8
+    Top = 8
+    Width = 129
+    Height = 107
+    Progress = 0
+  end
+  object btnLongQuery: TButton
+    Left = 176
+    Top = 32
+    Width = 377
+    Height = 57
+    Caption = 'Start long SQL Query'
+    TabOrder = 0
+    OnClick = btnLongQueryClick
+  end
+  object Timer1: TTimer
+    Enabled = False
+    Interval = 50
+    OnTimer = Timer1Timer
+    Left = 8
+    Top = 64
+  end
+end
---
+++ Main.pas
@@ -0,0 +1,103 @@
+﻿unit Main;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, System.Threading, Vcl.StdCtrls, Vcl.Samples.Gauges, Vcl.ExtCtrls;
+
+type
+  TForm4 = class(TForm)
+    GaugeAni: TGauge;
+    btnLongQuery: TButton;
+    Timer1: TTimer;
+    procedure btnLongQueryClick(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+    procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+  private
+    task: ITask;
+    procedure ShowProgress(text: string);
+    procedure AniEnabler(setstate: Boolean);
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form4: TForm4;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm4.btnLongQueryClick(Sender: TObject);
+begin
+  if Assigned(task) and (task.Status = TTaskStatus.Running) then
+    exit;
+
+  // run sql query in thread
+  task := TTask.Create(
+    procedure()
+    var
+      id: integer;
+    begin
+      // start animation
+      AniEnabler(true);
+
+      // start long query here ...
+      id := round(random(1000));
+      ShowProgress('thread id #' + id.ToString + ': start sql query 1');
+      Sleep(2000);
+      ShowProgress('thread id #' + id.ToString + ': start sql query 2');
+      Sleep(5000);
+      ShowProgress('thread id #' + id.ToString + ': start sql query 3');
+      Sleep(10000);
+      ShowProgress('thread id #' + id.ToString + ': end sql query');
+
+      // stop animation
+      AniEnabler(false);
+    end);
+
+  task.Start;
+
+end;
+
+procedure TForm4.ShowProgress(text: string);
+begin
+  TThread.Synchronize(nil,
+    procedure()
+    begin
+      caption := text;
+    end);
+end;
+
+procedure TForm4.AniEnabler(setstate: Boolean);
+begin
+  TThread.Synchronize(nil,
+    procedure()
+    begin
+      Timer1.Enabled := setstate;
+      if not setstate then
+        GaugeAni.Progress := 0;
+    end);
+end;
+
+procedure TForm4.FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+begin
+  if Assigned(task) and (task.Status = TTaskStatus.Running) then
+  begin
+    CanClose := false;
+    ShowMessage('Task in progress. Please wait');
+  end
+  else
+    CanClose := true;
+end;
+
+procedure TForm4.Timer1Timer(Sender: TObject);
+begin
+  GaugeAni.Progress := GaugeAni.Progress + 1;
+  if GaugeAni.Progress >= GaugeAni.MaxValue then
+    GaugeAni.Progress := GaugeAni.MinValue;
+end;
+
+end.
